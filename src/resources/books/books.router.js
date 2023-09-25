de584df48c9596b0d820d8f5e4e40f6f2fd40c6c
IvanGerman <ivgvi@mail.ru>
2023-09-25 12:59:07 +0700

    fix: delete some unnecessary modules

--- src/resources/books/books.router.js
+++ src/resources/books/books.router.js
@@ -1,18 +1,17 @@
 const express = require('express');
-const passport = require('passport');
 const controller = require('../../controllers/book');
 
 const router = express.Router();
 
 
 //the callback function under can be replaced by controller
-router.get('/books', passport.authenticate('jwt', { session: false }), controller.getBooks);
+router.get('/books', controller.getBooks);
 
-router.post('/books', passport.authenticate('jwt', { session: false }), controller.postBook);
+router.post('/books', controller.postBook);
 
-router.delete('/books/:id', passport.authenticate('jwt', { session: false }), controller.deleteBook);
+router.delete('/books/:id', controller.deleteBook);
 
-router.put('/books/:id', passport.authenticate('jwt', { session: false }), controller.updateBook);
+router.put('/books/:id',  controller.updateBook);
 
 
 
